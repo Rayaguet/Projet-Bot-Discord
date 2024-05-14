339a8b704dc75a52797a0dc5b208ea2ffd619036
Rayan <ray.guet@yahoo.fr>
2024-05-14 20:56:04 +0200

    ajout new config bot

--- main.js
+++ main.js
@@ -4,11 +4,20 @@ const bot = new Discord.Client({ intents });
 const loadCommands = require("./Loaders/loadCommands");
 const loadEvents = require("./Loaders/loadEvents");
 const config = require("./config");
+const mess_insert = require("./DataBase/mess_insert");
 
 bot.commands = new Discord.Collection();
 
-bot.login(config.token);
-loadCommands(bot);
-loadEvents(bot);
+bot.login(config.token).then(() => {
+    loadCommands(bot);
+    loadEvents(bot);
+    mess_insert(bot);
+    console.log("Fonction permettant l'insertion des messages en base fonctionnelle.");
+}).catch(err => {
+    console.error('Failed to login or initialize the bot:', err);
+});
+
+
+
 
 
